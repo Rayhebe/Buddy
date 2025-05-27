d62f2019f4e1b19560d0c92d1fe7842fdd86a9ef
Rayhebe <hebekuranyi@gmail.com>
2025-05-27 23:37:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ class Bot(BaseBot):
         self.user_loops = {}
         self.loop_emote_list = emote_list
 
+    async def on_user_join(self, user: User, position: Position | AnchorPosition) -> None:
+        # Only the bot prints the message in the console
+        print(f"{user.username} (ID: {user.id})")
+
+        # Announce the user has joined the room publicly
+        await self.highrise.chat(f"{user.username} joined to find a buddy!")
+
+        # Send welcome whispers to the user
+        await self.highrise.send_whisper(user.id, f"❤️Welcome [{user.username}]! Use: [!emote list] or [1-97] for dances & emotes.")
+        await self.highrise.send_whisper(user.id, f"❤️Use: [/help] for more information.")
+        await self.highrise.send_whisper(user.id, f"❤Type F3 F2 and F1 to teleport between the floor 🤍.")
+
+        # Send emotes
+        await self.highrise.send_emote("dance-hipshake")
+        await self.highrise.send_emote("emote-lust", user.id)
+
+       # React with a heart emoji
+        await self.highrise.react("heart", user.id)
+
     async def on_start(self, session_metadata: SessionMetadata) -> None:
         if bot_location:
             try:
