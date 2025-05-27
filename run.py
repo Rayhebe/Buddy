711eaf176b2cc1a9ed39d444ff1f3382907439ee
Rayhebe <hebekuranyi@gmail.com>
2025-05-27 21:32:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -21,11 +21,13 @@ class WebServer():
     t.start()
 
 
+
 class RunBot():
-  room_id = "6706b1dc20084804ef575ccb"
-  bot_token = "52e5102011c184340d8a7bc34620bea190aa3fd840a62db9a7eaf58c06e636f9"
+  room_id = "674e8734a6e32872a971aee8"
+  bot_token = "5435d9906d9d3d19f050f5fae9debd8c6c25da59cdd15ebe16ddecee22c90a88"
   bot_file = "main"
   bot_class = "Bot"
+
 
   def __init__(self) -> None:
     self.definitions = [
