1caac22d7ef372eec41185b41bf494b2146fedc4
Rayhebe <hebekuranyi@gmail.com>
2025-05-27 21:58:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import asyncio
 import random
 import os
 import importlib.util
-from highrise import*
-from highrise import BaseBot,Position
-from highrise.models import SessionMetadata
-from highrise import Highrise, GetMessagesRequest
-from highrise import BaseBot, Position
-from highrise.models import SessionMetadata, User, AnchorPosition
+
+from highrise import Highrise, BaseBot, Position
+from highrise.models import SessionMetadata, User, AnchorPosition, GetMessagesRequest
+
+# استيراد الدوال من ملفاتك
 from functions.loop_emote import (
     check_and_start_emote_loop,
     handle_user_movement,
