089da6f47f1ee3b1f47f98439f33a80271d8e737
Rayhebe <hebekuranyi@gmail.com>
2025-05-27 23:37:50 +0300

    Update emote_list_data.py

--- functions/emote_list_data.py
+++ functions/emote_list_data.py
@@ -1,4 +1,16 @@
-1. laidback  
-2. shy  
-3. cry  
-...
+# functions/emote_list_data.py
+
+emote_names = [
+    ("laidback", "LaidBack", 3),
+    ("clap", "Clap", 2),
+    ("dance", "Dance", 4),
+    ("laugh", "Laugh", 2),
+    ("sit", "Sit", 999),
+    ("wave", "Wave", 2),
+    ("idle", "Idle", 999),
+    ("cry", "Cry", 2),
+    ("sleep", "Sleep", 999),
+    ("jump", "Jump", 2),
+    ("wave2", "Wave2", 2),
+    ("angry", "Angry", 2)
+]
